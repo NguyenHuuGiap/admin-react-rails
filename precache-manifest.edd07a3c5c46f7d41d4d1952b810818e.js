e432cea7ae6e70679747757c6206676354920e99
Nguyễn Hữu Giáp <giapnh@bap.jp>
2019-04-05 15:39:57 +0700

    Updates

---
+++ precache-manifest.edd07a3c5c46f7d41d4d1952b810818e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6ff69c82cc37b323ceb5ff20224cc99",
+    "url": "/admin-react-rails/static/media/fa-brands-400.d6ff69c8.woff"
+  },
+  {
+    "revision": "892c5f4f9bf60475531e",
+    "url": "/admin-react-rails/static/css/main.3d853a16.chunk.css"
+  },
+  {
+    "revision": "92a059761ed87613c663",
+    "url": "/admin-react-rails/static/js/runtime~main.2fe980bb.js"
+  },
+  {
+    "revision": "aaab18e6bd0056b4032f",
+    "url": "/admin-react-rails/static/js/2.71c64de2.chunk.js"
+  },
+  {
+    "revision": "9c0eb6c2e967eccd837da618bcbde91c",
+    "url": "/admin-react-rails/static/media/fa-regular-400.9c0eb6c2.woff2"
+  },
+  {
+    "revision": "87f37798dcdf11a68de16eacfd566aa3",
+    "url": "/admin-react-rails/static/media/fa-regular-400.87f37798.woff"
+  },
+  {
+    "revision": "065edea411671c8caceecb999354cc21",
+    "url": "/admin-react-rails/static/media/fa-regular-400.065edea4.eot"
+  },
+  {
+    "revision": "2e562d4ebf15395f00bc738738f79291",
+    "url": "/admin-react-rails/static/media/fa-regular-400.2e562d4e.ttf"
+  },
+  {
+    "revision": "ae990e80be9a9904db60b0d3d06adbc1",
+    "url": "/admin-react-rails/static/media/fa-brands-400.ae990e80.woff2"
+  },
+  {
+    "revision": "52134b924fd61958f88323845deffc64",
+    "url": "/admin-react-rails/static/media/fa-solid-900.52134b92.woff2"
+  },
+  {
+    "revision": "892c5f4f9bf60475531e",
+    "url": "/admin-react-rails/static/js/main.9ecd6d6b.chunk.js"
+  },
+  {
+    "revision": "a6de637ca793ed8092597012d691a6a0",
+    "url": "/admin-react-rails/static/media/fa-solid-900.a6de637c.woff"
+  },
+  {
+    "revision": "1010b72339fe3292aede3543e827b741",
+    "url": "/admin-react-rails/static/media/fa-brands-400.1010b723.eot"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/admin-react-rails/static/media/fa-brands-400.c6aef942.ttf"
+  },
+  {
+    "revision": "5dc39cf0aed41e1a5e9918bea7e8603d",
+    "url": "/admin-react-rails/static/media/fa-regular-400.5dc39cf0.svg"
+  },
+  {
+    "revision": "a20e6745adc3184de8330b1f0579161f",
+    "url": "/admin-react-rails/static/media/fa-solid-900.a20e6745.eot"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/admin-react-rails/static/media/fa-solid-900.872545dd.ttf"
+  },
+  {
+    "revision": "c95f5f7a8923b8bb5034de9eb48901d7",
+    "url": "/admin-react-rails/static/media/fa-brands-400.c95f5f7a.svg"
+  },
+  {
+    "revision": "56404b18f3d1a3eaeed35b43343cfab9",
+    "url": "/admin-react-rails/static/media/fa-solid-900.56404b18.svg"
+  },
+  {
+    "revision": "8f4deafd7c1e50b8b1672646683ee4f2",
+    "url": "/admin-react-rails/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/admin-react-rails/precache-manifest.edd07a3c5c46f7d41d4d1952b810818e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/admin-react-rails/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
